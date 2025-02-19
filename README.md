342a782744583d62b470ac935132d5d236502b47
Julia Vinogradova <grapevine0112@yandex.ru>
2025-02-19 18:53:57 +0300

    add js and change README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/vishnevaya7/mesto-project-ff
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function createCard(cardData, deleteCardCallback) {
 
-// @todo: Функция удаления карточки
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteCardCallback(cardElement);
+    });
+
+    return cardElement;
+}
+
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+
+initialCards.forEach(card => {
+    const cardElement = createCard(card, deleteCard);
+    placesList.appendChild(cardElement);
+});
